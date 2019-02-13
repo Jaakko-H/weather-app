3ac70565b649ebef877cce9e3762116faa5cf170
Jaakko-H <jaheikela@gmail.com>
2019-02-13 22:03:13 +0200

    Added first test case to backend.

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,9 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || '';
-const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
-const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
+export const appId = process.env.APPID || '';
+export const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
+export const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
 
 const port = process.env.PORT || 9000;
 
@@ -13,29 +13,28 @@ const app = new Koa();
 
 app.use(cors());
 
-const fetchForecast = async (latitude, longitude) => {
+export const fetchForecast = async (latitude, longitude) => {
   const locationQueryParams = getLocationQueryParams(latitude, longitude);
   const endpoint = `${mapURI}/forecast?appid=${appId}${locationQueryParams}`;
-  console.debug('endpoint:', endpoint);
+  console.debug('endpoint: ', endpoint);
   const response = await fetch(endpoint);
 
   return response ? response.json() : {};
 };
 
-const fetchWeather = async (latitude, longitude) => {
+export const fetchWeather = async (latitude, longitude) => {
   const locationQueryParams = getLocationQueryParams(latitude, longitude);
   const endpoint = `${mapURI}/weather?appid=${appId}${locationQueryParams}`;
-  console.debug('endpoint:', endpoint);
   const response = await fetch(endpoint);
 
   return response ? response.json() : {};
 };
 
-const getLocationQueryParams = (latitude, longitude) => {
+export const getLocationQueryParams = (latitude, longitude) => {
   return latitude && longitude ? `&lat=${latitude}&lon=${longitude}` : `&q=${targetCity}`;
 };
 
-const attachCityNameIntoForecastEntity = (forecastEntity, cityName) => {
+export const attachCityNameIntoForecastEntity = (forecastEntity, cityName) => {
   forecastEntity.name = cityName;
   return forecastEntity;
 };
